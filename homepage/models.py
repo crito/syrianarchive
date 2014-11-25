35e6cd401b3a505028725e5e16f58a052fe3abcd
user <user@user>
2014-11-25 07:36:36 +0100

    fixed bug

--- homepage/models.py
+++ homepage/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django import forms
 from django.forms import ModelForm, Select
 from django.contrib.auth.models import User
-from profiles.models import Profile
 from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
 
 
