26b847834cd57941d22f75c77078d528ab1d360a
user <user@user>
2014-11-27 05:31:36 +0100

    major changes to model structure

--- database/admin.py
+++ database/admin.py
@@ -1,20 +1,87 @@
 from django.contrib import admin
 from database.models import *
 from django_summernote.admin import SummernoteModelAdmin
+from django.forms import CheckboxSelectMultiple
 
+@admin.register(InternationalInstrument)
+class InternationalInstrumentEntryAdmin(admin.ModelAdmin):
+	pass
 
+@admin.register(MediaContentType)
+class MediaContentTypeAdmin(admin.ModelAdmin):
+	pass
+
+@admin.register(SourceConnection)
+class SourceConnectionAdmin(admin.ModelAdmin):
+	pass
 
 @admin.register(DatabaseEntry)
 class DatabaseEntryAdmin(SummernoteModelAdmin):
+	formfield_overrides = {
+        models.ManyToManyField: {'widget': CheckboxSelectMultiple},
+    }
 	list_display = ('name', 
 		'graphic_content',
 		'staff_id',
 		'recording_date',
 		'online',
-		'content_type',
 		'location_name',
 		'priority',
 		)
+	fieldsets = (
+        ('Required Fields', {
+            'fields': ('name',)
+        }),
+        ('Public Fields', {
+            'fields': (
+            	'description',
+            	'reference_code', 
+            	'recording_date',
+            	'location_name', 
+            	('location_latitude','location_longitude',),
+            	'edited',
+            	('file_size','duration',),
+            	'chain_of_custody_notes_public',
+            	'media_content_type',
+            	'languages',
+            	'finding_aids',
+            	'graphic_content',
+            	'keywords',
+            	'international_instrument',
+            	'landmarks',
+            	'weather',
+            )
+        }),
+        ('Private Fields', {
+            'fields': (
+            	'staff_id',
+            	'file_name',
+            	'recording_creator',
+            	'generation',
+            	'location_of_original',
+            	'online',
+            	'online_link',
+            	'online_title',
+            	'date_of_acquisition',
+            	'acquired_from',
+            	'chain_of_custody_notes',
+            	'security_restriction_status',
+            	'security_restriction_notes',
+            	'date_of_fixity_check',
+            	'md5_hash',
+            	'rights_owner',
+            	'rights_declaration',
+            	'creator_willing_witness',
+            	'priority',
+            	'notes',
+            	'source_connection',
+            	'added_date',
+            	'creator',
+            	)
+        }),
+
+    )
+    
 	def save_model(self, request, obj, form, change):
 		if not change:
 			obj.creator = request.user
--- database/models.py
+++ database/models.py
@@ -5,83 +5,92 @@ from django.contrib.admin import widgets
 from datetime import datetime
 import time
 
-# Create your models here.
-'''
-    	-Reference Code
-    	-Staff ID
-    	-Date
-    	-Creator
-    	-File Name
-    	-Recording Date and Time
-    	-Location
-    	-Summary
-    	-Generation
-    	-Existence & Location of Original
-    	-Edited? (Y/N)
-    	-Is this footage online? (Y/N)
-    	-Online link
-    	-Online title
-    	-File Size
-    	-Duration
-    	-Date of acquisition
-    	-Acquired from
-    	-Chain of Custody Notes
-    	-Date of fixity check
-    	-MD5 hash value
-    	-Content Type
-    	-Language(s)
-    	-Finding Aids
-    	-Is there graphic content? (Y/N)
-    	-Security Restriction Status
-    	-Rights owner
-   		-Rights declaration
-    	-Creator willing to be a witness in case
-    	footage is used as evidence for a legal
-    	case? (Y/N)
-    	-Priority of the footage
-    	-Keywords
-    -Notes
-    '''
+class InternationalInstrument(models.Model):
+	name = models.CharField(max_length=250)
+	number = models.CharField(max_length=250)
 
-class DatabaseEntry(models.Model):
+	def __unicode__(self):
+		return self.name
+
+class MediaContentType(models.Model):
 	name = models.CharField(max_length=250)
 
+	def __unicode__(self):
+		return self.name
+
+class SourceConnection(models.Model):
+	name = models.CharField(max_length=250)
+
+	def __unicode__(self):
+		return self.name
+
+class DatabaseEntry(models.Model):
+
+	#public
+	name = models.CharField(max_length=250)
 	reference_code = models.CharField(max_length=250, null=True, blank=True)
-	staff_id = models.CharField(max_length=250, null=True, blank=True)
-	file_name = models.CharField(max_length=250, null=True, blank=True)
 	description = models.TextField(max_length=5000, null=True, blank=True)
-	recording_date = models.DateTimeField(default=datetime.now, auto_now_add=True, null=True, blank=True)
+	recording_date = models.DateTimeField(default=datetime.now, null=True, blank=True)
 	location_name = models.CharField(max_length=250, null=True, blank=True)
 	location_latitude = models.CharField(max_length=250, null=True, blank=True)
 	location_longitude = models.CharField(max_length=250, null=True, blank=True)
+	edited = models.NullBooleanField(null=True, blank=True)
+	file_size = models.CharField(max_length=250, null=True, blank=True)
+	duration = models.CharField(max_length=250, null=True, blank=True)
+	chain_of_custody_notes_public = models.TextField(max_length=5000, null=True, blank=True)
+	LANGS=(
+		('AR','Arabic'),
+		('EN','English'),
+		('FR','French'),
+		('RU','Russian'),
+		('FA','Persian'),
+	)
+	languages = models.CharField(max_length=250, null=True, blank=True, choices=LANGS)
+	finding_aids = models.CharField(max_length=250, null=True, blank=True)
+	graphic_content = models.NullBooleanField(null=True, blank=True)
+	keywords = models.CharField(max_length=250, null=True, blank=True)
+	international_instrument = models.ManyToManyField(InternationalInstrument, null=True, blank=True)
+	media_content_type = models.ManyToManyField(MediaContentType, null=True, blank=True)
+	landmarks = models.TextField(max_length=5000, null=True, blank=True)
+	weather = models.TextField(max_length=5000, null=True, blank=True)
+
+
+	
+
+	#private
+	staff_id = models.CharField(max_length=250, null=True, blank=True)
+	file_name = models.CharField(max_length=250, null=True, blank=True)
+	recording_creator = models.CharField(max_length=250, null=True, blank=True)
 	generation = models.CharField(max_length=250, null=True, blank=True)
 	location_of_original = models.CharField(max_length=250, null=True, blank=True)
-	edited = models.NullBooleanField(null=True, blank=True)
 	online = models.NullBooleanField(null=True, blank=True)
 	online_link = models.CharField(max_length=250, null=True, blank=True)
 	online_title = models.CharField(max_length=250, null=True, blank=True)
-	file_size = models.CharField(max_length=250, null=True, blank=True)
-	duration = models.CharField(max_length=250, null=True, blank=True)
 	date_of_acquisition = models.DateField(default=datetime.now)
 	acquired_from = models.CharField(max_length=250, null=True, blank=True)
 	chain_of_custody_notes = models.TextField(max_length=5000, null=True, blank=True)
-	date_of_fixity_check = models.DateField(default=datetime.now)
+	STATUS = (
+		('R', 'Restricted'),
+		('U', 'Unrestricted'),
+	)
+	security_restriction_status = models.CharField(max_length=250, null=True, blank=True, choices=STATUS)
+	security_restriction_notes = models.TextField(max_length=5000, null=True, blank=True)
+	date_of_fixity_check = models.DateTimeField(default=datetime.now, null=True, blank=True)
 	md5_hash = models.CharField(max_length=25000, null=True, blank=True)
-	content_type = models.CharField(max_length=250, null=True, blank=True)
-	languages = models.CharField(max_length=250, null=True, blank=True)
-	finding_aids = models.CharField(max_length=250, null=True, blank=True)
-	graphic_content = models.NullBooleanField(null=True, blank=True)
-	security_restriction_status = models.CharField(max_length=250, null=True, blank=True)
 	rights_owner = models.CharField(max_length=250, null=True, blank=True)
 	rights_declaration = models.CharField(max_length=250, null=True, blank=True)
 	creator_willing_witness = models.NullBooleanField(null=True, blank=True)
-	priority = models.CharField(max_length=250, null=True, blank=True)
-	keywords = models.CharField(max_length=250, null=True, blank=True)
+	PRIORITY_LIST = (
+        ('UR', 'Urgent'),
+        ('HP', 'High priority'),
+        ('P', 'Priority'),
+        ('LP', 'Low Priority'),
+        ('CR', 'Case Rejected'),
+    )
+	priority = models.CharField(max_length=250, null=True, blank=True, choices=PRIORITY_LIST)
 	notes = models.TextField(max_length=5000, null=True, blank=True)
-
-
 	added_date = models.DateField(default=datetime.now)
-
+	source_connection = models.ManyToManyField(SourceConnection, null=True, blank=True)
 	creator = models.ForeignKey(User, null=True, blank=True)
 
 	def __unicode__(self):
@@ -101,3 +110,6 @@ class DatabaseEntry(models.Model):
 
 	def save(self, *args, **kwargs):
 		super(DatabaseEntry, self).save(*args, **kwargs)
+
+
+
