c21f613cb70d1246534e3d9457d1d4fef401882b
user <user@user>
2014-12-09 00:37:30 +0100

    fixed content type template

--- database/admin.py
+++ database/admin.py
@@ -37,6 +37,7 @@ class DatabaseEntryAdmin(SummernoteModelAdmin):
             	'description',
             	'reference_code', 
             	'recording_date',
+            	'location_place',
             	'location_name', 
             	('location_latitude','location_longitude',),
             	'edited',	
