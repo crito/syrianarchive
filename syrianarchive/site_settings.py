9e6dfef9151b06f62ff296d5624299481b2ff2cb
user <user@user>
2015-04-07 17:56:33 +0200

    faads

--- syrianarchive/site_settings.py
+++ syrianarchive/site_settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = (
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'i18n.middleware.LocaleMiddleware',
+    'django.i18n.middleware.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
