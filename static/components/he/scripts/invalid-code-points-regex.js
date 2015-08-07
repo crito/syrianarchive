a159f1f409d0fea14e7e3fe295b33e53b859de0c
Niko <niko@niko.io>
2015-08-07 18:25:39 +0200

    deleted unneccesary js libs

--- static/components/he/scripts/invalid-code-points-regex.js
+++
@@ -1,6 +0,0 @@
-var regenerate = require('regenerate');
-
-var invalidRawCodePoints = require('../data/invalid-raw-code-points.json');
-var regexInvalidRawCodePoints = regenerate(invalidRawCodePoints).toString();
-
-module.exports = regexInvalidRawCodePoints;
